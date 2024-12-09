c80c4e54d21b42bbb73c77983ee40ad24645a782
Harikaran1212 <131746803+Harikaran1212@users.noreply.github.com>
2024-12-09 08:53:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PhaserGame from "./components/phaserGame";
 function App() {
   return (
     <div>
-      <h1 className="text">Harry Potter Spell Game</h1>
+      <h1 className="text" style={{ fontSize: "3rem"}}>Harry Potter Spell Game</h1>
       <PhaserGame />
     </div>
   );
